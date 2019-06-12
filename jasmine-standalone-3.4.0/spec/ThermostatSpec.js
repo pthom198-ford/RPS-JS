2914949915818d5db5c4e3256a226c66bb5c5fbd
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-06-12 15:16:39 +0100

    added min temp test

--- jasmine-standalone-3.4.0/spec/ThermostatSpec.js
+++ jasmine-standalone-3.4.0/spec/ThermostatSpec.js
@@ -13,8 +13,17 @@
     it ('can increase temp with up', function() {
     thermostat.uptemp()
     expect(thermostat.temp).toEqual(21)
-
     });
 
+    it ('can decrease temp with down', function() {
+    thermostat.downtemp()
+    expect(thermostat.temp).toEqual(19)
+    });
+
+    it ('have a minimum temp of 10', function() {
+    thermostat.temp = 10
+    thermostat.downtemp()
+    expect(thermostat.temp).toEqual(10)
+    });
   });
 });
--- jasmine-standalone-3.4.0/src/Thermostat.js
+++ jasmine-standalone-3.4.0/src/Thermostat.js
@@ -2,6 +2,10 @@ function Thermostat(temp) {
   this.temp = 20
 };
 
-Thermostat.prototype.uptemp = function(number) {
+Thermostat.prototype.uptemp = function() {
   this.temp = this.temp + 1
+};
+
+Thermostat.prototype.downtemp = function() {
+  this.temp = this.temp - 1
 };
