25367ee6bf681386b1f3e2e1f6d7b29474f5b114
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-06-16 12:43:08 +0100

    add logic

--- jasmine-standalone-3.4.0/src/Rockpaper.js
+++ jasmine-standalone-3.4.0/src/Rockpaper.js
@@ -3,3 +3,20 @@ function Rockpaper()) {
   this.playerChoice;
   this.computerChoice;
 };
+
+Rockpaper.prototype.play = function() {
+  if this.computerChoice === 'Rock' && this.playerChoice === 'Scissors'
+    then won
+  elsif this.computerChoice === 'Scissors' && this.playerChoice === 'Paper'
+    then won
+  elsif this.computerChoice === 'Paper' && this.playerChoice === 'Rock'
+    then won
+  elsif this.computerChoicee === 'Scissors' && this.playerChoice === 'Rock'
+    then lose
+  elsif this.computerChoice === 'Paper' && this.playerChoice === 'Scissors'
+    then lose
+  elsif this.computerChoice === 'Rock' && this.playerChoice === 'Paper'
+    then lose
+  else draw
+
+};
