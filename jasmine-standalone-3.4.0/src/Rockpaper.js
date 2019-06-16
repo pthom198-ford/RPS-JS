5180f8b6a1092bc551eb3a1e10c69092f78d1f6b
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-06-16 12:53:30 +0100

    change JS syntax

--- jasmine-standalone-3.4.0/src/Rockpaper.js
+++ jasmine-standalone-3.4.0/src/Rockpaper.js
@@ -5,20 +5,21 @@ function Rockpaper()) {
 };
 
 Rockpaper.prototype.play = function() {
-  if this.computerChoice === 'Rock' && this.playerChoice === 'Scissors'
-    then won
-  elsif this.computerChoice === 'Scissors' && this.playerChoice === 'Paper'
-    then won
-  elsif this.computerChoice === 'Paper' && this.playerChoice === 'Rock'
-    then won
-  elsif this.computerChoicee === 'Scissors' && this.playerChoice === 'Rock'
-    then lose
-  elsif this.computerChoice === 'Paper' && this.playerChoice === 'Scissors'
-    then lose
-  elsif this.computerChoice === 'Rock' && this.playerChoice === 'Paper'
-    then lose
-  else draw
-
+  if (this.computerChoice === 'Rock' && this.playerChoice === 'Scissors') {
+    return this.won
+  } else if (this.computerChoice === 'Scissors' && this.playerChoice === 'Paper') {
+    return this.won
+  } else if (this.computerChoice === 'Paper' && this.playerChoice === 'Rock') {
+    return this.won
+  } else if (this.computerChoicee === 'Scissors' && this.playerChoice === 'Rock') {
+    return this.lose
+  } else if (this.computerChoice === 'Paper' && this.playerChoice === 'Scissors') {
+    return this.lose
+  } else if (this.computerChoice === 'Rock' && this.playerChoice === 'Paper') {
+    return this.lose
+  } else {
+    return this.draw
+  };
 };
 
 Rockpaper.prototype.won = function() {
@@ -27,8 +28,8 @@ Rockpaper.prototype.won = function() {
 
 Rockpaper.prototype.lose = function() {
   return 'Player beats Computer'
-}
+};
 
 Rockpaper.prototype.draw = function() {
   return 'Its a draw'
-}
+};
